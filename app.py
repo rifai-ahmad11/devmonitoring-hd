de3e7f93f866bf19fea9a89ccd914c6039f031bb
rifai-ahmad11 <160980992+rifai-ahmad11@users.noreply.github.com>
2026-02-11 14:43:23 +0700

    Update app.py
    hapus debug=true

--- app.py
+++ app.py
@@ -441,4 +441,5 @@ cleanup_thread.start()
 
 if __name__ == '__main__':
     print("Starting Machine Monitoring Server...")
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+
+    socketio.run(app, host='0.0.0.0', port=5000)
